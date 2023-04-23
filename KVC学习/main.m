223ceacae85747dd9cfa149a6d63994e9694f283
zhaixubo <113840032+zhaixubo@users.noreply.github.com>
2023-04-23 21:38:09 +0800

    Initial Commit

---
+++ KVC学习/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  KVC学习
+//
+//  Created by 翟旭博 on 2023/4/23.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
